2cb900c182db52c74078d74f51a2783397fbfc67
tcjwj2008@126.com <tcjwj2008@126.com>
2020-05-07 10:18:59 +0800

    Update 肉制品修改固定资产明细表使用寿命出现错行现象　规格有空格引起.sql

--- 肉制品修改固定资产明细表使用寿命出现错行现象　规格有空格引起.sql
+++ 肉制品修改固定资产明细表使用寿命出现错行现象　规格有空格引起.sql
@@ -1,10 +1,28 @@
-SELECT * FROM t_facard WHERE fassetnumber='RZPQT0148'
-UPDATE t_FACard  SET fmodel='长6180*宽600*高900' WHERE FAssetNumber='RZPQT0148'
-SELECT * FROM t_facard WHERE fassetnumber='RZPQT0149'
-UPDATE t_FACard  SET fmodel='长2300*宽600' WHERE FAssetNumber='RZPQT0149'
-SELECT * FROM t_FABalCard WHERE fassetnumber='RZPQT0148'
-UPDATE dbo.t_FABalCard SET fmodel='长6180*宽600*高900' WHERE FAssetNumber='RZPQT0148'
-SELECT * FROM t_FABalCard WHERE fassetnumber='RZPQT0149'
-UPDATE dbo.t_FABalCard SET fmodel='长2300*宽600' WHERE FAssetNumber='RZPQT0149'
+SELECT  *
+FROM    t_FACard
+WHERE   FAssetNumber = 'RZPQT0148';
+UPDATE  t_FACard
+SET     FModel = '长6180*宽600*高900'
+WHERE   FAssetNumber = 'RZPQT0148';
+SELECT  *
+FROM    t_FACard
+WHERE   FAssetNumber = 'RZPQT0149';
+UPDATE  t_FACard
+SET     FModel = '长2300*宽600'
+WHERE   FAssetNumber = 'RZPQT0149';
+SELECT  *
+FROM    t_FABalCard
+WHERE   FAssetNumber = 'RZPQT0148';
+UPDATE  dbo.t_FABalCard
+SET     FModel = '长6180*宽600*高900'
+WHERE   FAssetNumber = 'RZPQT0148';
+SELECT  *
+FROM    t_FABalCard
+WHERE   FAssetNumber = 'RZPQT0149';
+UPDATE  dbo.t_FABalCard
+SET     FModel = '长2300*宽600'
+WHERE   FAssetNumber = 'RZPQT0149';
 
 
+SELECT fmodel,RTRIM(fmodel) FROM t_FABalCard WHERE fmodel<>RTRIM(fmodel)
+SELECT fmodel,RTRIM(fmodel) FROM t_FACard WHERE fmodel<>RTRIM(fmodel)
