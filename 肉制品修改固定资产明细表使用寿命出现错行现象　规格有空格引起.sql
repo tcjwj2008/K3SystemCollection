c32bb7db778cd0cf3bf10e551cec399178f21be3
tcjwj2008@126.com <tcjwj2008@126.com>
2020-05-07 10:00:35 +0800

    Create 肉制品修改固定资产明细表使用寿命出现错行现象　规格有空格引起.sql

---
+++ 肉制品修改固定资产明细表使用寿命出现错行现象　规格有空格引起.sql
@@ -0,0 +1,10 @@
+SELECT * FROM t_facard WHERE fassetnumber='RZPQT0148'
+UPDATE t_FACard  SET fmodel='长6180*宽600*高900' WHERE FAssetNumber='RZPQT0148'
+SELECT * FROM t_facard WHERE fassetnumber='RZPQT0149'
+UPDATE t_FACard  SET fmodel='长2300*宽600' WHERE FAssetNumber='RZPQT0149'
+SELECT * FROM t_FABalCard WHERE fassetnumber='RZPQT0148'
+UPDATE dbo.t_FABalCard SET fmodel='长6180*宽600*高900' WHERE FAssetNumber='RZPQT0148'
+SELECT * FROM t_FABalCard WHERE fassetnumber='RZPQT0149'
+UPDATE dbo.t_FABalCard SET fmodel='长2300*宽600' WHERE FAssetNumber='RZPQT0149'
+
+
