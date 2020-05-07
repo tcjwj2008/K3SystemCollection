083b1f24847f3d8452d0580bbcf79e9621d3e18c
tcjwj2008@126.com <tcjwj2008@126.com>
2020-05-07 08:33:17 +0800

    add file
    add file

--- 2020-04-15猪ＯＫ平台需要使用Ｋ３账套　己删除关键数据.sql
+++ 2020-04-15猪ＯＫ平台需要使用Ｋ３账套　己删除关键数据.sql
@@ -15,26 +15,31 @@ TRUNCATE TABLE t_rp_contact;
 TRUNCATE TABLE icbal;
 --TRUNCATE TABLE icbalance;
 
-SELECT * FROM t_rp_contact;
-
-SELECT * FROM icbal;
-SELECT * FROM icbalance;
-SELECT * FROM t_rp_contract
-SELECT * FROM t_rpcontract;
-SELECT * FROM t_rpcontractdetail
-SELECT * FROM  seorder;
-SELECT * FROM icsale;
-
+USE test_yxspzhuok;
+DELETE FROM dbo.t_Voucher WHERE fdate<'2020-03-01';
 SELECT * FROM dbo.t_Voucher WHERE fdate<'2020-03-01';
 DELETE FROM dbo.t_Voucher WHERE fdate<'2020-03-01';
 SELECT * FROM    dbo.t_Voucher ,t_voucherentry WHERE dbo.t_Voucher.FVoucherID=dbo.t_VoucherEntry.FVoucherID
-
 AND fdate<'2020-03-01';
 
 DELETE FROM t_voucherentry  FROM dbo.t_Voucher ,t_voucherentry WHERE dbo.t_Voucher.FVoucherID=dbo.t_VoucherEntry.FVoucherID
-
-AND fdate>='2020-03-01';
+AND fdate<='2020-03-01';
 
 SELECT *  FROM t_voucherentry WHERE FVoucherID NOT IN (SELECT FVoucherID FROM t_voucher);
 DELETE FROM t_voucherentry WHERE FVoucherID NOT IN (SELECT FVoucherID FROM t_voucher);
 
+/*验证删除情况
+
+SELECT * FROM t_rp_contact;
+
+SELECT * FROM icbal;
+SELECT * FROM icbalance;
+SELECT * FROM t_rp_contract;
+SELECT * FROM t_rpcontract;
+SELECT * FROM t_rpcontractdetail;
+SELECT * FROM  seorder;
+SELECT * FROM icsale;
+
+
+
+*/
